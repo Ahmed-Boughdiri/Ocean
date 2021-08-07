d522b5292ecbef9feb32db4729ab4b65b4146805
Ahmed Boughdiri <ahmedboughdiri2020@gmail.com>
2021-08-07 15:27:54 +0100

    Setting Up Basic Socket Communication

---
+++ public/js/index.js
@@ -0,0 +1,94 @@
+const socket = io("http://localhost:5000/");
+const rooms = [
+    {
+        roomID: "1",
+        messages: [],
+        dom: document.querySelectorAll(".room")[0]
+    },
+    {
+        roomID: "2",
+        messages: [],
+        dom: document.querySelectorAll(".room")[1]
+    },
+];
+let joined_room;
+
+const chatboxInput = document.querySelector(".chatbox-input>input");
+const chatboxSendButton = document.querySelector(".chatbox-input>i"); 
+const chatboxMessages = document.querySelector(".chatbox-messages");
+
+socket.on("sent-message", message =>{
+    const roomIndex = rooms.findIndex(
+        room => 
+            room.roomID === joined_room.roomID
+    );
+    if(roomIndex != -1) {
+        rooms[roomIndex].messages.push(message);
+        renderMessage({
+            message,
+            sender: "Unknown",
+            isFirst: (rooms[roomIndex].messages) ? true : false
+        })
+    }
+});
+
+function renderMessage({
+    sender,
+    message,
+    isFirst=false
+}) {
+    const component = `
+        <div class="chatbox-message-container ${(sender === "user") && "sent-by-user"} ${isFirst && "first"}">
+            ${
+                (sender !== "user") ?
+                `<div class="chatbox-message-arrow ${(sender === "user") && "sent-by-user"}"></div>` : 
+                ""
+            }
+            <div class="chatbox-message ${(sender === "user") && "sent-by-user"}">
+                <h3 class="chatbox-message-owner">
+                    ${(sender === "user") ? "You" : sender}
+                </h3>
+                <p class="chatbox-message-content">
+                    ${message}
+                </p>
+            </div>
+            ${
+                (sender === "user") ?
+                `<div class="chatbox-message-arrow ${(sender === "user") && "sent-by-user"}"></div>` : 
+                ""
+            }
+        </div>
+    `;
+    chatboxMessages.innerHTML += component;
+}
+
+rooms.forEach(room =>{
+    room.dom.addEventListener("click", e =>{
+        socket.emit("join-room", room.roomID);
+        joined_room = rooms.filter(
+            roomData => 
+                roomData.roomID === room.roomID
+        )[0];
+    });
+});
+
+chatboxSendButton.addEventListener("click", e =>{
+    const message = chatboxInput.value;
+    socket.emit(
+        "chat-message", 
+        { 
+            room: joined_room.roomID, 
+            message 
+        }
+    );
+    const roomIndex = rooms.findIndex(room => room.roomID === joined_room.roomID);
+    if(roomIndex != -1) {
+        rooms[roomIndex].messages.push(message);
+        renderMessage({
+            message,
+            sender: "user",
+            isFirst: (rooms[roomIndex].messages) ? true : false
+        });
+        chatboxInput.value = "";
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { json } from "express";
 import path from "path";
+import { Server } from "socket.io";
+import http from "http";
 
+// SETTING UP THE EXPRESS SERVER
 const app = express();
+const server = http.createServer(app);
 const PORT = 5000;
 app.use(json());
 app.use(express.static(path.join(__dirname, '../public')));
@@ -10,7 +14,20 @@ app.get("/", (req, res) =>{
     return res.render("index");
 });
 
-app.listen(
+server.listen(
     process.env.PORT || PORT, 
     () => console.log(`SERVER RUNNING ON PORT ${PORT}...`)
 );
+
+// SETTING UP THE SOCKET SERVER
+const io = new Server(server);
+
+io.on("connection", socket => {
+    socket.on("join-room", roomID =>{
+        console.log(`User Have Joined Room ${roomID}`);
+        socket.join(roomID);
+    });
+    socket.on("chat-message", data =>{
+        socket.to(data.room).emit("sent-message", data.message);
+    });
+});
